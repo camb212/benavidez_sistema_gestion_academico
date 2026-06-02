f727e8f12a2eb5b22f40a6b600b166205a86daba
camb212 <camila.benavidez@ute.edu.ec>
2026-06-02 05:27:41 -0500

    cambios

--- estudiantes_api/catalog/urls.py
+++ estudiantes_api/catalog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import PlanViewSet, SocioViewSet
+from .services_views import cobros_view, clases_view
 
 router = DefaultRouter()
-router.register(r"planes", PlanViewSet,  basename="planes")
-router.register(r"socios", SocioViewSet, basename="socios")
+router.register(r"datos", PlanViewSet,  basename="datos")
+router.register(r"estudiante", SocioViewSet, basename="estudiante")
 
-urlpatterns = []
+urlpatterns = [
+    path("estudiante/matricula/", cobros_view),   # POST — FOR
+    path("nota/aprobado/", clases_view),   # GET  — WHILE
+]
+
 urlpatterns += router.urls
